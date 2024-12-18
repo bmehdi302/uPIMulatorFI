d0c68999d4fc9e334c99e14b9803340e4a1a4271
bmehdi302 <ebelhaddad@bordeaux-inp.fr>
2024-12-18 14:32:10 +0100

    Update analyze.py

--- iram_FI/uPIMulator/python_cpp/uPIMulator_backend/FI/analyze.py
+++ iram_FI/uPIMulator/python_cpp/uPIMulator_backend/FI/analyze.py
@@ -1,1 +1,24 @@
+# Analyze the results of the fault injection
+def analyze_fault_injection_results(log_file_path):
+    with open(log_file_path, "r") as file:
+        lines = file.readlines()
 
+    completed = 0
+    errors = 0
+    crashes = 0
+    timeouts = 0
+
+    for line in lines:
+        if "Result: completed" in line:
+            completed += 1
+        elif "Result: error" in line:
+            errors += 1
+        elif "Result: crash" in line:
+            crashes += 1
+        elif "Result: timeout" in line:
+            timeouts+=1
+
+    print(f"Total Completed: {completed}")
+    print(f"Total Errors: {errors}")
+    print(f"Total Crashes: {crashes}")
+    print(f"Total timeouts: {timeouts}")
