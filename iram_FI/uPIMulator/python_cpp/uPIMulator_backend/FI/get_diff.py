0a4eeb0af5e007cd3fa8c319ff018866d8d11f20
bmehdi302 <ebelhaddad@bordeaux-inp.fr>
2024-12-18 14:29:42 +0100

    Update get_diff.py

--- iram_FI/uPIMulator/python_cpp/uPIMulator_backend/FI/get_diff.py
+++ iram_FI/uPIMulator/python_cpp/uPIMulator_backend/FI/get_diff.py
@@ -1,11 +1,10 @@
 import re
 
 def get_differences_from_log(log_file):
-    differences = 0  # Default value if not found
     """
     Get number of differences from execution output log file
     """
-
+    differences = 0  # Default value if not found
     with open(log_file, 'r') as infile:
         for line in infile:
             # Look for the "DIFFERENCES:" line and extract the number
