3560f24fcdc44889e1a510cc02402287590bb562
bmehdi302 <ebelhaddad@bordeaux-inp.fr>
2024-12-18 16:21:47 +0100

    Update get_addresses.py

--- IRAM_bit_flip/uPIMulator_backend/FI/get_addresses.py
+++ IRAM_bit_flip/uPIMulator_backend/FI/get_addresses.py
@@ -2,6 +2,6 @@ def get_unique_addresses(filename):
   """
   get accessed addresses from .txt file
   """
-    with open(filename, "r") as file:
-        addresses = {int(line.strip()) for line in file}  
-    return list(addresses)
+  with open(filename, "r") as file:
+    addresses = {int(line.strip()) for line in file}  
+  return list(addresses)
